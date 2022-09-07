214965ff383f1a42ecf813fa45d46e07bce74800
JorgeArmandoSB <jorgesilvacreativo@gmail.com>
2022-09-06 22:23:31 -0500

    tarjeta con el html dentro del javascript

--- main.js
+++ main.js
@@ -1,2 +1,26 @@
+let container = document.querySelector("#main");
 
+container.insertAdjacentHTML("beforeend",`
+
+        <div class="card">
+            <div class="imgBx">
+            <img src="./img/christina-wocintechchat-com-0Zx1bDv5BNY-unsplash.jpg">
+            </div>
+            <div class="content">
+                <div class="details">
+                    <h2>alina smith<br><span>Senior UX/UI Desinger</span></h2>
+                         <div class="data">
+                        <h3>342<br><span>Posts</span></h3>
+                        <h3>120K<br><span>Follower</span></h3>
+                        <h3>285<br><span>Following</span></h3>
+                        </div>
+                    <div class="actionBtn">
+                        <button>Follow</button>
+                        <button>Message</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+`)
 
