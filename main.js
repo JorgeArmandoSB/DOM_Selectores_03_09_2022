d01df25ffbd90cfa357d23b74715b220064984d4
JorgeArmandoSB <jorgesilvacreativo@gmail.com>
2022-09-08 18:44:12 -0500

    cambios

--- main.js
+++ main.js
@@ -4,19 +4,19 @@ container.insertAdjacentHTML("beforeend",`
 
         <div class="card">
             <div class="imgBx">
-            <img src="./img/christina-wocintechchat-com-0Zx1bDv5BNY-unsplash.jpg">
+            <img src="./img/profile-1.jpeg">
             </div>
             <div class="content">
                 <div class="details">
-                    <h2>alina smith<br><span>Senior UX/UI Desinger</span></h2>
+                    <h2>Jorge Silva<br><span>Diseñador Gráfico</span></h2>
                          <div class="data">
                         <h3>342<br><span>Posts</span></h3>
-                        <h3>120K<br><span>Follower</span></h3>
-                        <h3>285<br><span>Following</span></h3>
+                        <h3>120K<br><span>Seguidores</span></h3>
+                        <h3>285<br><span>Seguidos</span></h3>
                         </div>
                     <div class="actionBtn">
-                        <button>Follow</button>
-                        <button>Message</button>
+                        <button>Seguir</button>
+                        <button>Mensaje</button>
                     </div>
                 </div>
             </div>
